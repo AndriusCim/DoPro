0139efc42364a53d3ad30178184edd5c7c93b4d3
Andrius Cimermonas <Andrius.Cimermonas@bdo.global>
2020-04-23 14:44:07 +0300

    More changes

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <header>
+            <h1>DoPro</h1>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/views/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+
+import '../styles/styles.css';
+
+const App: React.FC = () => {
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    )
+}
+
+export default App;
