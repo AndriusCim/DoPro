fb5262b6359fee04882f64bdcbe66df3afca506a
Andrius Cimermonas <Andrius.Cimermonas@bdo.global>
2020-05-04 22:04:54 +0300

    Scss fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { Icon, Heading, Button } from "evergreen-ui";
 const Header: React.FC = () => {
   return (
     <>
-      <div className="TopBar cs-d-flex">
+      <div className="header cs-d-flex">
         <Icon icon="tint" color="47B881" size={30} />
         <Heading size={500}>CORONA STATS</Heading>
         <Button className="cs-ml-lg" appearance="minimal" intent="success">
